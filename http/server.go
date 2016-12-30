0c899e6277e03bbd6a14167f5dbdf00b29a94407
管宜尧 <guanyiyao@yunsom.com>
2016-12-30 17:57:55 +0800

    http实现修改，添加supervisor配置

--- http/server.go
+++ http/server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/mylxsw/task-runner/config"
 	"github.com/mylxsw/task-runner/console"
 	"github.com/mylxsw/task-runner/http/handler"
@@ -12,19 +13,17 @@ import (
 
 // StartHTTPServer start an http server instance serving for api request
 func StartHTTPServer() {
-
-	// print welcome message
-	http.HandleFunc("/", handler.Home)
-	// check the server status
-	http.HandleFunc("/status", handler.Status)
-	// push task to task queue
-	http.HandleFunc("/push", handler.PushTask)
-	// create new task queue
-	http.HandleFunc("/queue", handler.NewQueue)
-
 	runtime := config.GetRuntime()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler.Home).Methods("GET")
+	r.HandleFunc("/status", handler.Status).Methods("GET")
+	r.HandleFunc("/push", handler.PushTask).Methods("POST")
+	r.HandleFunc("/queue", handler.NewQueue).Methods("POST")
+
 	log.Debug("http listening on %s", console.ColorfulText(console.TextCyan, runtime.Config.HTTP.ListenAddr))
-	if err := http.ListenAndServe(runtime.Config.HTTP.ListenAddr, nil); err != nil {
+
+	if err := http.ListenAndServe(runtime.Config.HTTP.ListenAddr, r); err != nil {
 		log.Error("failed listening http on %s: %v", runtime.Config.HTTP.ListenAddr, err)
 		os.Exit(2)
 	}
